77e04c1f96cb943ce74135a71ef6b2e579062865
Jacob Downs <jdowns@umass.edu>
2016-12-06 21:48:27 -0500

    re-triangle

---
+++ projects/triangle/test/triangle/TriangleTest.java
@@ -0,0 +1,23 @@
+package triangle;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static triangle.Triangle.Type;
+import static triangle.Triangle.Type.*;
+
+/**
+ * Test class for the Triangle implementation.
+ */
+public class TriangleTest {
+
+    /*
+     * Test the classification of an equilateral triangle.
+     */
+    @Test
+    public void test1() {
+        Type actual = Triangle.classify(1, 1, 1);
+        Type expected = EQUILATERAL;
+        assertEquals (expected, actual);
+    }
+}
